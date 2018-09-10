7b24f376670c93521f030336fa3b91208c0b70d3
boonich <43152280+boonich@users.noreply.github.com>
2018-09-11 01:15:42 +0200

    Add files via upload

---
+++ launch.py
@@ -0,0 +1,9 @@
+#launch file for running app in qpython (change paths according to your android system)
+
+#import subprocess
+
+#pysh = "/data/user/0/org.qpython.qpy/files/bin/qpython-android5.sh"
+
+#callpy = "/sdcard/qpython/projects/eeswebgl/EESWebGL.py"
+
+#subprocess.call([pysh, callpy])
